b06d8406e44d58bda68a0e76449d418229962622
Chris Breneman <crispy@cluenet.org>
2015-03-14 17:38:17 -0400

    More common query

---
+++ test/queries.js
@@ -0,0 +1,104 @@
+var expect = require('chai').expect;
+var createQuery = require('../lib/index').createQuery;
+
+describe('Query', function() {
+
+	describe('matches()', function() {
+
+		it('basic exact match', function(done) {
+			let query1 = createQuery({
+				foo: 'bar',
+				biz: 'baz'
+			});
+			expect(query1.matches({
+				foo: 'bar',
+				biz: 'baz',
+				qux: 'buz'
+			})).to.equal(true);
+			expect(query1.matches({
+				foo: 'bar'
+			})).to.equal(false);
+			expect(query1.matches({
+				foo: 'bar',
+				biz: 'bam',
+				qux: 'buz'
+			})).to.equal(false);
+			done();
+		});
+
+		it('full object exact match', function(done) {
+			let query1 = {
+				foo: {
+					bar: 'biz',
+					baz: 'buz'
+				}
+			};
+			expect(query1.matches({
+				foo: {
+					bar: 'biz',
+					baz: 'buz'
+				},
+				zip: 'fuz'
+			})).to.equal(true);
+			expect(query1.matches({
+				foo: {
+					bar: 'biz',
+					baz: 'buz',
+					qux: 'bam'
+				},
+				zip: 'fuz'
+			})).to.equal(false);
+			done();
+		});
+
+		it('combined $and, $or, $nor', function(done) {
+			let query1 = createQuery({
+				$and: [
+					{
+						foo: 'bar'
+					},
+					{
+						biz: 'baz'
+					}
+				],
+				$or: [
+					{
+						qux: 'buz'
+					},
+					{
+						bam: 'fuz'
+					}
+				],
+				$nor: [
+					{
+						zip: 'foo'
+					}
+				]
+			});
+			expect(query1.matches({
+				foo: 'bar',
+				biz: 'baz',
+				qux: 'buz'
+			})).to.equal(true);
+			expect(query1.matches({
+				foo: 'bar',
+				qux: 'buz'
+			})).to.equal(false);
+			expect(query1.matches({
+				foo: 'bar',
+				biz: 'baz',
+				qux: 'buz',
+				zip: 'foo'
+			})).to.equal(false);
+			expect(query1.matches({
+				foo: 'bar',
+				biz: 'baz'
+			})).to.equal(false);
+			done();
+		});
+
+	});
+
+});
+
+
