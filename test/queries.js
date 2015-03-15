b9a3a96913f5c7bbb8194a719f09d518fcbf0fff
Chris Breneman <crispy@cluenet.org>
2015-03-15 15:45:02 -0400

    More geo operators

--- test/queries.js
+++ test/queries.js
@@ -518,6 +518,49 @@ describe('Query', function() {
 			})).to.equal(true);
 			expect(query1.getMatchProperty('distance')).to.be.above(500);
 			expect(query1.getMatchProperty('distance')).to.be.below(800);
+			expect(query1.matches({
+				loc: {
+					type: 'Point',
+					coordinates: [ -84.5087746, 39.0972566 ]
+				}
+			})).to.equal(true);
+			expect(query1.matches({
+				loc: [
+					{
+						type: 'Point',
+						coordinates: [ -84.5087746, 39.0972566 ]
+					},
+					{
+						type: 'Point',
+						coordinates: [ -84.168767, 39.1413997 ]
+					}
+				]
+			})).to.equal(true);
+			done();
+		});
+
+		it('$geoIntersects', function(done) {
+			let query1 = createQuery({
+				poly: {
+					$geoIntersects: {
+						$geometry: {
+							type: 'Point',
+							coordinates: [ -84.5099628, 39.1031535 ] // 602 Main St 45202
+						}
+					}
+				}
+			});
+			expect(query1.matches({
+				poly: {
+					type: 'Polygon',
+					coordinates: [ [
+						[ -84.51316, 39.1052099 ],
+						[ -84.5058322, 39.1053431 ],
+						[ -84.5101237, 39.1004809 ],
+						[ -84.51316, 39.1052099 ]
+					] ]
+				}
+			})).to.equal(true);
 			done();
 		});
 
