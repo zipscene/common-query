66e8c9b2dc09ca9ca80e3254c682c73e30fedde4
Kevin <kevin.breneman@zipscene.com>
2015-03-19 15:22:40 -0400

    and , also moved validation to update constructor

--- test/update.js
+++ test/update.js
@@ -37,4 +37,56 @@ describe('Update apply()', function() {
 		expect(objtools.deepEquals(newObj, expectedObj)).to.equal(true);
 		done();
 	});
+
+	it('$inc', function(done) {
+		let obj = {
+			one: 1,
+			eightpointfive: 8.5,
+			thirteen: 13,
+			primes: [ 2, 3, 5, 7, 11 ]
+		};
+		let update = {
+			$inc: {
+				one: 0,
+				eightpointfive: 1.3,
+				thirteen: -8,
+				'primes.1': 4
+			}
+		};
+		let newObj = createUpdate(update).apply(obj);
+		let expectedObj = {
+			one: 1,
+			eightpointfive: 9.8,
+			thirteen: 5,
+			primes: [ 2, 7, 5, 7, 11 ]
+		};
+		expect(objtools.deepEquals(newObj, expectedObj)).to.equal(true);
+		done();
+	});
+
+	it('$mul', function(done) {
+		let obj = {
+			one: 1,
+			eightpointfive: 8.5,
+			thirteen: 13,
+			primes: [ 2, 3, 5, 7, 11 ]
+		};
+		let update = {
+			$mul: {
+				one: 0,
+				thirteen: -3,
+				'primes.1': 10
+			}
+		};
+		let newObj = createUpdate(update).apply(obj);
+		let expectedObj = {
+			one: 0,
+			eightpointfive: 8.5,
+			thirteen: -39,
+			primes: [ 2, 30, 5, 7, 11 ]
+		};
+		expect(objtools.deepEquals(newObj, expectedObj)).to.equal(true);
+		done();
+	});
+
 });
